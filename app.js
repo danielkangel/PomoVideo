2f21c2fc05ecb1e68c6033404d84c69dcb129dd5
Daniel Angel <dka190000@utdallas.edu>
2021-08-09 04:20:49 -0500

    added session storage and moved middleware

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const path = require('path');
 const methodOverride = require('method-override');
 const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const defTimer = require("./models/default");
 if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 
 app.set('views', path.join(__dirname, 'views'));
@@ -11,10 +13,17 @@ app.set('view engine', 'ejs');
 app.use(session({
     secret: process.env.SECRET_KEY,
     resave: false,
-    saveUninitialized: false,
-    cookie: {secure: true, httpOnly: true, sameSite: 'none'}
+    saveUninitialized: true,
+    cookie: {secure: false, sameSite: 'strict'},
+    store: MongoStore.create({ mongoUrl: process.env.MONGO_URL})
 }));
 app.use(express.static(__dirname + '/public'));
+app.use((req, res, next) => {
+    if (!req.session.settings) {
+        req.session.settings = defTimer;
+    }
+    next();
+});
 app.use(methodOverride('_method'));
 app.use(require('./routes'));
 
